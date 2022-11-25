28640e8e113b43b51d5e8648a1408ba3dd32faff
diegotimao <diegotimao1104@gmail.com>
2022-11-25 17:57:44 -0500

    feat: Verificando theme darkmode footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../provider/auth';
 import './footer.css';
 
 function Footer() {
+
+  const { theme } = useContext(AuthContext);
+  const [themeAplication, setTeamAplication] = useState('');
+
+  useEffect(() => {
+    if (theme === 'lithe') return setTeamAplication('footer-container-lithe-theme ');
+    return setTeamAplication('footer-container-dark-theme');
+  }, [theme]); 
+
   return (
-    <section className="footer-container">
+    <section className={ themeAplication }>
       <div className="footer-content">
         <strong>Desenvolvido por Diego Cavalcanti</strong>
       </div>
