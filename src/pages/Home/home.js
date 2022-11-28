a0ae8ba44b786b8ed7e2fef2457e862106af2764
diegotimao <diegotimao1104@gmail.com>
2022-11-27 21:51:33 -0500

    feat: Retirando lib aos do Home

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,7 +1,4 @@
-import React, { useEffect } from 'react';
-import Aos from "aos";
-import "aos/dist/aos.css";
-
+import React from 'react';
 import Banner from '../../components/Banner';
 import Sobre from '../../components/Sobre';
 import Tecks from '../../components/Tecks';
@@ -11,9 +8,7 @@ import Contato from '../../components/Contato';
 import Footer from '../../components/Footer';
 
 function Home() {
-  useEffect(() => {
-    Aos.init({ duration: 2000 });
-  }, [])
+  
 
   return (
     <main>
