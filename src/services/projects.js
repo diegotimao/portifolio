2e1da17d008ddadb59554d5c83859f6d247089dd
diegotimao <diegotimao1104@gmail.com>
2022-11-27 21:51:51 -0500

    fix: Corrigindo ortografia

--- src/services/projects.js
+++ src/services/projects.js
@@ -21,14 +21,14 @@ const projects = [
   {
     image: recipes,
     title: 'Recipes App',
-    description: 'Aplicativo de receitas consumindo api de comidas e bebidas, desenvolvida em ResactJs utilizando o redux.',
+    description: 'Aplicativo de receitas consumindo api de comidas e bebidas, desenvolvida em React utilizando o redux.',
     urlPreviwer: 'https://recipes-app-red-eight.vercel.app/foods',
     urlCode: 'https://github.com/diegotimao/recipes-app',
   },
   {
     image: pixelsImage,
     title: 'Pixels arte',
-    description: 'Paleta de cores em javascript vanilha com manipulação de eventos e funções puras do javascript.',
+    description: 'Paleta de cores em javascript vanilla com manipulação de eventos e funções puras do javascript.',
     urlPreviwer: 'https://trybe-projeto-pixels-art.vercel.app/',
     urlCode: 'https://github.com/diegotimao/trybe-projeto-pixels-art',
   }
