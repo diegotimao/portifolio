1487dad04de7df32223a7e882ee432121e48f15c
diegotimao <diegotimao1104@gmail.com>
2023-04-13 22:13:33 -0500

    feat: Adicionando novas tecnologias

--- src/components/Tecks/index.js
+++ src/components/Tecks/index.js
@@ -1,20 +1,25 @@
 import React, { useContext, useState, useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import { AuthContext } from "../../provider/auth";
 import tecks from '../../services/tecks';
 import './teck.css';
- 
-function Techs() {
 
+function Techs() {
   const { theme } = useContext(AuthContext);
   const [themeAplication, setTeamAplication] = useState('');
 
   useEffect(() => {
     if (theme === 'lithe') return setTeamAplication('teck-container-lithe-theme');
     return setTeamAplication('teck-container-dark-theme');
-  }, [theme]); 
- 
+  }, [theme]);
+
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, [])
+
   return (
-    <section id="tecks" className={ themeAplication }>
+    <section id="tecks" className={themeAplication}>
       <div className="teck-content">
         <div className="teck-info">
           <h1>Tecnologias</h1>
@@ -23,19 +28,19 @@ function Techs() {
         <div className="teck-list">
           <div className="list">
             {tecks.map((teck) => (
-              <div key={ teck.teckname } className="teck-item" data-aos="fade-right">
-                <img src={ teck.icon } alt={ teck.teckname } />
+              <div key={teck.teckname} className="teck-item" data-aos="fade-up">
+                <img src={teck.icon} alt={teck.teckname} />
                 <div className="teck-info-project">
-                  <h2>{ teck.teckname }</h2>
-                  <strong>{ teck.description }</strong>
+                  <h2>{teck.teckname}</h2>
+                  <strong>{teck.description}</strong>
                 </div>
               </div>
-            ))} 
+            ))}
           </div>
         </div>
       </div>
     </section>
   )
-} 
+}
 
 export default Techs;
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Aos from 'aos';
+import "aos/dist/aos.css";
 import Banner from '../../components/Banner';
 import Sobre from '../../components/Sobre';
 import Tecks from '../../components/Tecks';
@@ -8,7 +10,9 @@ import Contato from '../../components/Contato';
 import Footer from '../../components/Footer';
 
 function Home() {
-  
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, [])
 
   return (
     <main>
