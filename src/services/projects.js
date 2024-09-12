d88f1e8464d5cca21ffee5221dab49d469aa8f32
Diego <diegotimao1104@gmail.com>
2024-09-12 16:24:16 -0300

    feat: Melhorando a descrição de projetos

--- src/services/projects.js
+++ src/services/projects.js
@@ -7,31 +7,32 @@ const projects = [
   {
     image: trybetunes,
     title: 'MusicFy',
-    description: 'Pesquise por seus albuns e artistas favoritos, dá até para reproduzir uma prévia da música.',
+    description: 'Explore seus álbuns e artistas favoritos em uma interface intuitiva que permite ouvir prévias das músicas diretamente na aplicação. Perfeito para entusiastas de música que buscam descobrir novos ritmos e sons.',
     urlPreviwer: 'https://music-songs.vercel.app/',
     urlCode: 'https://github.com/diegotimao/music-songs',
   },
   {
     image: trivia,
     title: 'Jogo de Trivia',
-    description: 'Através desta aplicação o usuário se diverte com um jogo de perguntas e respostas, sua pontuação é calculada em tempo real bem como o ranking.',
+    description: 'Desafie seus conhecimentos com um jogo dinâmico de perguntas e respostas. A pontuação é calculada em tempo real, e você pode competir com outros jogadores em um ranking interativo.',
     urlPreviwer: 'https://jogo-trivia-sandy.vercel.app/',
     urlCode: 'https://github.com/diegotimao/jogo-trivia',
   },
   {
     image: recipes,
     title: 'Recipes App',
-    description: 'Aplicativo de receitas consumindo api de comidas e bebidas, desenvolvida em React utilizando o redux.',
+    description: 'Descubra uma ampla variedade de receitas de comidas e bebidas com este aplicativo desenvolvido em React e Redux. Ideal para quem gosta de explorar novas receitas com praticidade.',
     urlPreviwer: 'https://recipes-app-red-eight.vercel.app/foods',
     urlCode: 'https://github.com/diegotimao/recipes-app',
   },
   {
     image: pixelsImage,
-    title: 'Pixels arte',
-    description: 'Paleta de cores em javascript vanilla com manipulação de eventos e funções puras do javascript.',
+    title: 'Pixels Arte',
+    description: 'Crie obras de arte pixeladas com uma paleta de cores personalizável usando apenas JavaScript puro. Este projeto destaca a manipulação de eventos e funções nativas do JavaScript para uma experiência criativa.',
     urlPreviwer: 'https://trybe-projeto-pixels-art.vercel.app/',
     urlCode: 'https://github.com/diegotimao/trybe-projeto-pixels-art',
   }
 ];
+
 
 export default projects;
